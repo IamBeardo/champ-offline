03c9d5c5a6a9bf9c0a50905d609adf706f91fa63
IamBeardo <adam@rajraj.se>
2017-06-09 10:12:49 +0200

    Add files via upload

---
+++ Test/Test.py
@@ -0,0 +1,30 @@
+
+
+
+import champions
+import csv
+
+
+
+t = champions.champ.fromName("APAN")
+
+print (t.Name)
+print (t)
+
+
+#with open('names.csv') as csvfile:
+#...     reader = csv.DictReader(csvfile)
+#...     for row in reader:
+#...         print(row['first_name'], row['last_name']
+
+
+#csvreader = csv.DictReader(open("c:\champions (1).csv"))
+
+#for apa in csvreader:
+#    pass
+#    apa=champions.champ.fromHookCSV(apa)
+#    a="alsdkjf"
+
+roster = champions.champList()
+roster.loadHookCsv("c:\champions (1).csv")
+
---
+++ Test/champions.py
@@ -0,0 +1,50 @@
+import csv
+
+class champ(object):
+    """Object of a MCOC champion"""
+
+    @classmethod
+    def fromHookCSV(cls, row):
+        pass
+        print (row)
+        print ("")
+
+        return cls(row["Id"],int(row["Stars"]),int(row["Rank"]),int(row["Level"]),int(row["Awakened"]),[row["Role"]])
+        #OrderedDict([('Id', 'scarletwitch'), ('Stars', '4'), ('Rank', '3'), ('Level', '30'), ('Awakened', '0'), ('Pi', '0'), ('Role', '')])
+
+    def __init__(self, Name="None", Stars=1, Rank=1,Level=1, Sig=0, Tags=None):
+        self.Name   = Name
+        self.Stars  = Stars*1
+        self.Rank   = Rank
+        self.Level  = Level
+        self.Sig    = Sig*1
+        self.Awakened= Sig!=0
+        self.Prestige = None  # needs lookup
+
+        if Tags is None:
+            self.Tags = []
+        else:
+            self.Tags = Tags
+
+    def __str__(self):
+        pass
+        return " [{}] {} {} ".format('*'* self.Stars,self.Name,self.Rank)
+
+
+class champList(object):
+    """list of champs"""
+
+    
+    def __init__(self):
+        self.champs = dict()
+
+
+    def loadHookCsv(self,f):
+        csvreader = csv.DictReader(open(f))
+        
+        for row in csvreader:
+            newchamp=champ.fromHookCSV(row)
+            self.champs["{} {}".format(newchamp.Stars,newchamp.Name)]=newchamp
+      
+        return self
+
