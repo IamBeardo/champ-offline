42e162822682092e210d59e771ebe4f039ffad8b
IamBeardo <adam@rajraj.se>
2017-06-09 10:59:08 +0200

    1

--- Test/Test.py
+++ Test/Test.py
@@ -4,29 +4,6 @@
 import champions
 import csv
 
-
-
-#t = champions.champ.fromName("APAN")
-
-#print (t)
-
-
-#with open('names.csv') as csvfile:
-#...     reader = csv.DictReader(csvfile)
-#...     for row in reader:
-#...         print(row['first_name'], row['last_name']
-
-
-#csvreader = csv.DictReader(open("c:\champions (1).csv"))
-
-#for apa in csvreader:
-#    pass
-#    apa=champions.champ.fromHookCSV(apa)
-#    a="alsdkjf"
-
 roster = champions.champList()
 roster.loadHookCsv("c:\champions (1).csv")
-
-#asdf
-
 
